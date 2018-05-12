4fc8697be8b20ae080fd90e9352353f5bc1ef56b
Mateusz Szwankowski <mateusz.szwankowski@poczta.fm>
2018-05-12 15:05:53 +0200

    add 'member since' to profile

--- core/models.py
+++ core/models.py
@@ -3,6 +3,7 @@ from datetime import date
 from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from django.utils import timezone
 
 from registration.signals import user_registered
 from sorl.thumbnail import ImageField
@@ -13,6 +14,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50, blank=True)
     birth_date = models.DateField(null=True, blank=True)
+    join_date = models.DateTimeField()
     country = models.CharField(max_length=20, blank=True)
     city = models.CharField(max_length=35, blank=True)
     bio = models.CharField(max_length=220, blank=True)
@@ -21,7 +23,7 @@ class Profile(models.Model):
     @staticmethod
     @receiver(user_registered)
     def create(sender, user, request, **kwargs):
-        profile = Profile(user=user)
+        profile = Profile(user=user, join_date=timezone.now())
         profile.save()
 
     def location(self):
